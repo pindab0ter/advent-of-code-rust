d3236ad626819c67bacb4e44ef51a2a99535bd34
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2026-07-01 21:00:53 +0200

    Solve 2025 day 1 part 2

--- 2015/day01/src/main.rs
+++ 2015/day01/src/main.rs
@@ -3,18 +3,37 @@ use aoc_client::input;
 fn main() {
     let instructions = input(2015, 1);
 
-    let result: i32 = find_floor(&instructions);
-    println!("The instructions take Santa to floor {result}");
+    let floor = find_floor(&instructions);
+    println!("The instructions take Santa to floor {floor}");
+
+    let instruction_index = find_when_entering_basement(&instructions);
+    println!("The basement was eneterd at instruction #{instruction_index}")
 }
 
 fn find_floor(instructions: &str) -> i32 {
-    instructions.bytes().fold(0, |acc, char| {
-        acc + match char {
+    instructions.bytes().fold(0, |acc, instruction| {
+        acc + match instruction {
             b'(' => 1,
             b')' => -1,
             _ => panic!("Invalid character"),
         }
     })
+}
+
+fn find_when_entering_basement(instructions: &str) -> i32 {
+     instructions
+        .bytes()
+        .scan(0, |floor, instruction| {
+            *floor += match instruction {
+                b'(' => 1,
+                b')' => -1,
+                _ => panic!("Invalid character"),
+            };
+            Some(*floor)
+        })
+        .position(|floor| floor == -1)
+        .map(|index| index + 1)
+        .unwrap() as i32
 }
 
 #[cfg(test)]
@@ -34,5 +53,12 @@ mod tests {
     #[case(")())())", -3)]
     fn finds_the_correct_floor(#[case] input: &str, #[case] expected: i32) {
         assert_eq!(find_floor(input), expected);
+    }
+
+    #[rstest]
+    #[case(")", 1)]
+    #[case("()())", 5)]
+    fn finds_when_the_basement_is_entered(#[case] input: &str, #[case] expected: i32) {
+        assert_eq!(find_when_entering_basement(input), expected);
     }
 }
