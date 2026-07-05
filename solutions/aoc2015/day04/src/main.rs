15e810e4b654c09f58c592407212b44f40314cbf
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2026-07-05 20:30:12 +0200

    Solve 2015 day 4 part 2

--- solutions/aoc2015/day04/src/main.rs
+++ solutions/aoc2015/day04/src/main.rs
@@ -1,17 +1,30 @@
 use aoc_client::input;
 use common_macros::timed;
+use md5::digest::Output;
 use md5::{Digest, Md5};
 use std::iter::successors;
 
 fn main() {
     let input = input(2015, 4);
 
-    let result = mine_advent_coin(&input);
-    println!("Advent Coin mined at {result}");
+    let first_five_leading_zeroes = mine_advent_coin(&input, has_five_leading_hex_zeroes);
+    println!("Advent Coin starting with 5 zeroes mined at {first_five_leading_zeroes}\n");
+
+    let first_six_leading_zeroes = mine_advent_coin(&input, has_six_leading_hex_zeroes);
+    println!("Advent Coin starting with 6 zeroes mined at {first_six_leading_zeroes}");
 }
 
+fn has_five_leading_hex_zeroes(value: Output<Md5>) -> bool {
+    value[..2] == [0, 0] && value[2] & 0xf0 == 0
+}
+
+fn has_six_leading_hex_zeroes(value: Output<Md5>) -> bool {
+    value[..3] == [0, 0, 0]
+}
+
+// TODO: Improve performance with paralellization
 #[timed]
-fn mine_advent_coin(secret_key: &str) -> u32 {
+fn mine_advent_coin<F: Fn(Output<Md5>) -> bool>(secret_key: &str, is_valid_advent_coin: F) -> u32 {
     let mut secret_key_bytes = secret_key.as_bytes().to_vec();
     let base = secret_key_bytes.len();
     let mut hasher = Md5::new();
@@ -23,8 +36,7 @@ fn mine_advent_coin(secret_key: &str) -> u32 {
         Digest::update(&mut hasher, &secret_key_bytes);
         let digest = Digest::finalize_reset(&mut hasher);
 
-        // Each byte holds two hex digits
-        if digest[..2] == [0, 0] && digest[2] & 0xf0 == 0 {
+        if is_valid_advent_coin(digest) {
             return n;
         }
     }
@@ -40,6 +52,9 @@ mod tests {
     #[case("abcdef", 609043)]
     #[case("pqrstuv", 1048970)]
     fn mines_advent_coins(#[case] input: &str, #[case] expected: u32) {
-        assert_eq!(mine_advent_coin(input), expected);
+        assert_eq!(
+            mine_advent_coin(input, has_five_leading_hex_zeroes),
+            expected
+        );
     }
 }
