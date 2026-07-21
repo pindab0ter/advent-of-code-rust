218f60014c22edfef368326a200638723c32a40a
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2026-07-21 18:04:26 +0200

    Simplify 2015 day 9 by reusing existing Grid data type

--- solutions/aoc2015/day09/src/main.rs
+++ solutions/aoc2015/day09/src/main.rs
@@ -4,84 +4,47 @@ use aoc_client::input;
 use common::timed;
 use common_macros::timed;
 use itertools::Itertools;
-use std::convert::Infallible;
-use std::ops::{Index, IndexMut};
-use std::str::FromStr;
+use common::grid::Grid;
 
 type Distance = u32;
 
-/// [`distances`] is a 2D matrix of distances indexed by
-/// `source_city_id + [city_count] * destination_city_id`. See [`distance_index`].
-#[derive(Debug)]
-struct DistanceMatrix {
-    city_count: usize,
-    distances: Vec<Option<Distance>>,
-}
+#[timed]
+fn parse(s: &str) -> Grid<Distance> {
+    let cities_to_distances = s
+        .lines()
+        .map(|line| {
+            let (source, rest) = line.split_once(" to ").unwrap();
+            let (destination, distance) = rest.split_once(" = ").unwrap();
 
-impl DistanceMatrix {
-    fn new(n: usize) -> Self {
-        Self {
-            city_count: n,
-            distances: vec![None; n * n],
-        }
+            ((source, destination), distance.parse().unwrap())
+        })
+        .collect::<Vec<((&str, &str), Distance)>>();
+
+    let mut cities = cities_to_distances
+        .iter()
+        .flat_map(|((source, destination), _)| [*source, *destination])
+        .collect::<Vec<&str>>();
+    cities.sort();
+    cities.dedup();
+
+    let mut grid = Grid::new(cities.len(), cities.len());
+
+    for ((source, destination), distance) in cities_to_distances {
+        let source_id = cities.binary_search(&source).unwrap();
+        let destination_id = cities.binary_search(&destination).unwrap();
+        grid[(source_id, destination_id)] = distance;
+        grid[(destination_id, source_id)] = distance;
     }
-}
 
-impl Index<(usize, usize)> for DistanceMatrix {
-    type Output = Option<Distance>;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.distances[index.0 * self.city_count + index.1]
-    }
-}
-
-impl IndexMut<(usize, usize)> for DistanceMatrix {
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        &mut self.distances[index.0 * self.city_count + index.1]
-    }
-}
-
-impl FromStr for DistanceMatrix {
-    type Err = Infallible;
-
-    #[timed]
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let cities_to_distances = s
-            .lines()
-            .map(|line| {
-                let (source, rest) = line.split_once(" to ").unwrap();
-                let (destination, distance) = rest.split_once(" = ").unwrap();
-
-                ((source, destination), distance.parse().unwrap())
-            })
-            .collect::<Vec<((&str, &str), Distance)>>();
-
-        let mut cities = cities_to_distances
-            .iter()
-            .flat_map(|((source, destination), _)| [*source, *destination])
-            .collect::<Vec<&str>>();
-        cities.sort();
-        cities.dedup();
-
-        let mut matrix = DistanceMatrix::new(cities.len());
-
-        for ((source, destination), distance) in cities_to_distances {
-            let source_id = cities.binary_search(&source).unwrap();
-            let destination_id = cities.binary_search(&destination).unwrap();
-            matrix[(source_id, destination_id)] = Some(distance);
-            matrix[(destination_id, source_id)] = Some(distance);
-        }
-
-        Ok(matrix)
-    }
+    grid
 }
 
 fn main() {
     let input = input(2015, 9);
-    let distance_matrix = input.parse().unwrap();
+    let grid = parse(&input);
 
     let (shortest_route, longest_route) = timed("calculating shortest and longest route", || {
-        route_distances(&distance_matrix)
+        route_distances(&grid)
             .minmax()
             .into_option()
             .unwrap()
@@ -91,17 +54,13 @@ fn main() {
     println!("The longest route visiting all destinations is: {longest_route}");
 }
 
-fn distance_index(destination_id: usize, source_id: usize, city_count: usize) -> usize {
-    source_id * city_count + destination_id
-}
-
-fn route_distances(matrix: &DistanceMatrix) -> impl Iterator<Item = Distance> {
-    let all_possible_routes = (0..matrix.city_count).permutations(matrix.city_count);
+fn route_distances(grid: &Grid<Distance>) -> impl Iterator<Item = Distance> {
+    let all_possible_routes = (0..grid.width).permutations(grid.width);
 
     all_possible_routes.map(|route| {
         route
             .windows(2)
-            .map(|hop| matrix.distances[distance_index(hop[0], hop[1], matrix.city_count)].unwrap())
+            .map(|hop| grid[(hop[0], hop[1])])
             .sum::<Distance>()
     })
 }
@@ -119,22 +78,16 @@ mod tests {
 
     #[test]
     fn finds_the_shortest_route() {
-        let distance_matrix = EXAMPLE.parse().unwrap();
-        let (shortest_route, _) = route_distances(&distance_matrix)
-            .minmax()
-            .into_option()
-            .unwrap();
+        let grid = parse(EXAMPLE);
+        let (shortest_route, _) = route_distances(&grid).minmax().into_option().unwrap();
 
         assert_eq!(shortest_route, 605);
     }
 
     #[test]
     fn finds_the_longest_route() {
-        let distance_matrix = EXAMPLE.parse().unwrap();
-        let (_, longest_route) = route_distances(&distance_matrix)
-            .minmax()
-            .into_option()
-            .unwrap();
+        let grid = parse(EXAMPLE);
+        let (_, longest_route) = route_distances(&grid).minmax().into_option().unwrap();
 
         assert_eq!(longest_route, 982);
     }
